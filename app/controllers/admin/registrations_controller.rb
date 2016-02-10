1836f06129a7231b75822f4afc52f1443c9efd99
Francis Joe <francis.tm@gmail.com>
2016-02-10 14:33:05 +0800

    can change current password now

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -4,10 +4,19 @@ class Admin::RegistrationsController < AdminController
   end
 
   def update
+    password_changed = false
+
     @admin = current_admin
-    @admin.attributes = admin_params
+    @admin.attributes = admin_attribute_params
+
+    if admin_password_params[:password].present?
+      password_changed = true
+      @admin.attributes = admin_password_params
+    end
 
     if @admin.valid? && @admin.save
+      sign_in @admin, bypass: true if password_changed
+
       flash[:success] = t 'admin/registrations.admin_update_success'
       redirect_to [:edit, :admin, :registration] and return
     end
@@ -16,11 +25,16 @@ class Admin::RegistrationsController < AdminController
     render action: :edit
   end
 
-  private
-
-  def admin_params
+  def admin_attribute_params
     params.require(:admin).permit(
       :nickname
+    )
+  end
+
+  def admin_password_params
+    params.require(:admin).permit(
+      :password,
+      :password_confirmation
     )
   end
 end
