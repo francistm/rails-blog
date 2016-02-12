e43a2038f91204425d364993fe1a4d9b141cf951
Francis Joe <francis.tm@gmail.com>
2016-02-13 03:33:11 +0800

    add pygments.rb highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,37 @@ module ApplicationHelper
 
   private
 
+  class HTMLWithPygments < Redcarpet::Render::HTML
+    def block_code(code, lang)
+      lang = "text" if lang.blank?
+      Pygments.highlight code, lexer: lang
+    end
+  end
+
   def flash_mesg_box(klass, mesg)
     content_tag :div, mesg, class: "alert alert-#{klass}"
   end
 
   def markdown_parser
     @markdown_parser ||= Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML.new({
-      }),
-      {
-      }
+      markdown_parser_render,
+      markdown_parser_options
     )
+  end
+
+  def markdown_parser_render
+    HTMLWithPygments.new({
+      hard_wrap: true,
+      filter_html: true,
+      link_attributes: { rel: 'external nofollow' },
+    })
+  end
+
+  def markdown_parser_options
+    {}.tap do |h|
+      h[:quote] = true
+      h[:autolink] = true
+      h[:fenced_code_blocks] = true
+    end
   end
 end
