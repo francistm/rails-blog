251de318df539a1de07b2cd0c24c40ee42fb29fc
Francis Joe <francis.tm@gmail.com>
2016-02-24 22:52:54 +0800

    move rails-settings default to seeds

--- config/initializers/rails_settings_defaults.rb
+++
@@ -1,2 +0,0 @@
-SiteSetting.save_default :site_name, ''
-SiteSetting.save_default :feed_output_count, 5
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
+SiteSetting.save_default :site_name, ''
+SiteSetting.save_default :feed_output_count, 5
+
 Admin.create!(email: 'francis.tm@gmail.com', password: '123456789', nickname: 'admin') unless Admin.exists?
