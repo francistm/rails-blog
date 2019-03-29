c8c0cd69380684c47fd54a98f0064cf741389bc1
Francis <francistm.zhou@data-whisper.com>
2019-03-29 18:46:44 +0800

    switch to chrome for system test

--- spec/system/admins/posts_spec.rb
+++ spec/system/admins/posts_spec.rb
@@ -1,17 +1,13 @@
 require 'faker'
 require 'rails_helper'
 
-describe "Create Post", type: :system do
+describe "Post form", type: :system do
   before :each do
     login_as create(:admin), scope: :admin
   end
 
   after :each do
     Warden.test_reset!
-  end
-
-  before :all do
-    driven_by :selenium, using: :firefox
   end
 
   it 'submit without upload file' do
@@ -35,16 +31,26 @@ describe "Create Post", type: :system do
   it 'submit with upload file' do
     visit new_admins_post_url
 
+    post_slug = Faker::Internet.slug
+    post_title = Faker::Lorem.sentence
     post_attach_file = File.expand_path("../../../fixtures/files/file-image-fixture.jpg", __FILE__)
 
     assert_selector "h1", text: "添加新日志"
 
+    fill_in 'Slug', with: post_slug
+    fill_in '标题', with: post_title
     page.find('input#post_uploads.file-hidden-input', visible: :hidden).attach_file post_attach_file
+    fill_in '正文', with: Faker::Markdown.random
 
     expect(page).to have_text("file-image-fixture.jpg")
+
+    click_button '新增日志'
+
+    expect(page).to have_text(post_slug)
+    expect(page).to have_text(post_title)
   end
 
-  it 'delete file after upload before submit' do
+  it 'delete upload before submit' do
     visit new_admins_post_url
 
     post_attach_file = File.expand_path("../../../fixtures/files/file-image-fixture.jpg", __FILE__)
