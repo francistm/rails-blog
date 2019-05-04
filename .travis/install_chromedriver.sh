59273973ec0323b978fff500739c3a9b27b31bdc
francis <francis.tm@gmail.com>
2019-05-04 22:50:49 +0800

    修改travis中依赖安装的脚本

--- .travis/install_chromedriver.sh
+++ .travis/install_chromedriver.sh
@@ -5,6 +5,6 @@ CHROME_STABLE_VER=74.0.3729.6
 INSTALL_PREFIX=/usr/bin
 REMOTE_URL=https://chromedriver.storage.googleapis.com/${CHROME_STABLE_VER}/chromedriver_linux64.zip
 
-wget $REMOTE_URL -P /tmp
+wget --quiet $REMOTE_URL -P /tmp
 sudo unzip /tmp/chromedriver_linux64.zip -d $INSTALL_PREFIX
 sudo chmod +x $INSTALL_PREFIX/chromedriver
