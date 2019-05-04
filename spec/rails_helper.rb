4d3a3ee84875d6327f815b4bbf699f452a8bbca7
francis <francis.tm@gmail.com>
2019-05-04 18:40:04 +0800

    rspec中增加多张图片上传测试

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,6 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start 'rails'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -33,6 +30,14 @@ require 'rspec/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+begin
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start 'rails'
+rescue StandardError => e
   puts e.to_s.strip
   exit 1
 end
