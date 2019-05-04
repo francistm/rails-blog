f6eecad3baf33dc3c2b8c61073f753e8d52cc585
francis <francis.tm@gmail.com>
2019-05-04 21:22:32 +0800

    更新站点设置功能

--- app/controllers/admins/settings_controller.rb
+++ app/controllers/admins/settings_controller.rb
@@ -1,11 +1,16 @@
 class Admins::SettingsController < AdminController
   def index
-    @site_settings = SiteSetting.unscoped
+    @site_settings = setting_list.map do |setting_var|
+      {}.tap do |h|
+        h[:var] = setting_var
+        h[:value] = SiteSetting.send(setting_var)
+      end
+    end
   end
 
   def update
-    setting_params[:site_setting].each do |id, setting|
-      SiteSetting.send "#{setting[:var]}=", setting[:value]
+    setting_params.each do |value|
+      SiteSetting.send "#{value[0]}=", value[1]
     end
 
     flash[:success] = t 'admin/settings.setting_update_success'
@@ -14,9 +19,14 @@ class Admins::SettingsController < AdminController
 
   private
 
+  def setting_list
+    [].tap do |arr|
+      arr << :site_name
+      arr << :feed_output_count
+    end
+  end
+
   def setting_params
-    params.require(:settings).permit(
-      site_setting: [:var, :value]
-    )
+    params.require(:site_setting).permit(*setting_list)
   end
 end
